28ad4d125ea3a6222635dad7eca4f1fdc83d1587
joeljovi <joeljoviios@gmail.com>
2025-10-13 13:16:33 +0530

    Initial Commit

---
+++ eSociety/eSocietyApp.swift
@@ -0,0 +1,17 @@
+//
+//  eSocietyApp.swift
+//  eSociety
+//
+//  Created by Joel on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct eSocietyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
